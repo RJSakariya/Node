f30d48fc236c6830b0c1e47e3ad2d95a7548941d
RJSakariya <rjsakariya5@gmail.com>
2025-02-24 17:32:38 +0530

    Finel Project

--- Final Exam/controller/taskHandler.js
+++ Final Exam/controller/taskHandler.js
@@ -2,6 +2,9 @@ const taskSchema = require('../model/taskSchema')
 
 module.exports.viewTask = async (req, res) => {
     await taskSchema.findOne({ user: req.user.id }).then((data) => {
-        res.render('taskItem', { tasks: data,role:req.user.role})
+        res.render('taskItem', { tasks: data, role: req.user.role })
     })
+}
+module.exports.taskForm = async (req, res) => {
+    res.render('taskForm', { id: req.user.id })
 }
--- Final Exam/routes/taskRoute.js
+++ Final Exam/routes/taskRoute.js
@@ -4,5 +4,6 @@ const handler = require('../controller/taskHandler')
 const check = require('../middleware/jwtChecker')
 
 route.get('/', check.check, handler.viewTask)
+route.get('/taskForm', check.check, handler.taskForm)
 
 module.exports = route
